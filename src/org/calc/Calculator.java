f2037af8f94375488f737b524392ac9c0c30ebfd
oooleks <oooleks@gmail.com>
2020-12-27 20:53:55 -0600

    New code try

--- src/org/calc/Calculator.java
+++ src/org/calc/Calculator.java
@@ -5,128 +5,129 @@ import java.awt.GridLayout;
 import java.awt.BorderLayout;
 
 public class Calculator {
-	// Объявление всех компонентов калькулятора.
-	JPanel windowContent;
-	JFormattedTextField displayField, displayField2;
-	JButton buttonPoint;
-	JButton buttonEqual;
-	JButton buttonPlus;
-	JButton buttonMinus;
-	JButton buttonDivide;
-	JButton buttonMultiply;
-	JButton buttonSqrt;
-	JButton buttonClear;
-	JPanel p1, p2, p3;
-	
-	
+    // Объявление всех компонентов калькулятора.
+    JPanel windowContent;
+    JFormattedTextField displayField, displayField2;
+    JButton buttonPoint;
+    JButton buttonEqual;
+    JButton buttonPlus;
+    JButton buttonMinus;
+    JButton buttonDivide;
+    JButton buttonMultiply;
+    JButton buttonSqrt;
+    JButton buttonClear;
+    JPanel p1, p2, p3;
+
+
 // В конструкторе создаются все компоненты
 // и добавляются на фрейм с помощью комбинации
 // Borderlayout и Gridlayout
 
-Calculator(){
-windowContent = new JPanel();
+    Calculator() {
+        windowContent = new JPanel();
 
 // Задаём схему для этой панели
-BorderLayout bl = new BorderLayout();
-windowContent.setLayout(bl);
+        BorderLayout bl = new BorderLayout();
+        windowContent.setLayout(bl);
 
 // Создаём и отображаем поле
 // Добавляем его в Северную область окна
-displayField = new JFormattedTextField();
-displayField.setHorizontalAlignment(JFormattedTextField.RIGHT);
-windowContent.add("North",displayField);
-displayField2 = new JFormattedTextField();
-displayField2.setHorizontalAlignment(JFormattedTextField.RIGHT);
+        displayField = new JFormattedTextField();
+        displayField.setHorizontalAlignment(JFormattedTextField.RIGHT);
+        windowContent.add("North", displayField);
+        displayField2 = new JFormattedTextField();
+        displayField2.setHorizontalAlignment(JFormattedTextField.RIGHT);
 
 // Создаём кнопки, используя конструктор
 // класса JButton, который принимает текст
 // кнопки в качестве параметра
-JButton[] numButtons= new JButton[10];
-String b;
-for (int i=0; i<10; i++) {
-	b = Integer.toString(i);
-	numButtons[i] = new JButton(b);
-}
+        JButton[] numButtons = new JButton[10];
+        String b;
+        for (int i = 0; i < 10; i++) {
+            b = Integer.toString(i);
+            numButtons[i] = new JButton(b);
+        }
 
-buttonPoint   = new JButton(".");
-buttonEqual   = new JButton("=");
-buttonPlus    = new JButton("+");
-buttonMinus   = new JButton("-");
-buttonDivide  = new JButton("/");
-buttonMultiply= new JButton("*");
-buttonSqrt    = new JButton("SQRT");
-buttonClear   = new JButton("C");
+        buttonPoint = new JButton(".");
+        buttonEqual = new JButton("=");
+        buttonPlus = new JButton("+");
+        buttonMinus = new JButton("-");
+        buttonDivide = new JButton("/");
+        buttonMultiply = new JButton("*");
+        buttonSqrt = new JButton("SQRT");
+        buttonClear = new JButton("C");
 
 // Создаём панель с GridLayout
 // которая содержит 12 кнопок - 10 кнопок с числами
 // и кнопки с точкой и знаком равно
-p1 = new JPanel();
-GridLayout gl1 = new GridLayout(4,3);
-p1.setLayout(gl1);
+        p1 = new JPanel();
+        GridLayout gl1 = new GridLayout(4, 3);
+        p1.setLayout(gl1);
 
-p2 = new JPanel();
-GridLayout gl2 = new GridLayout(4,1);
-p2.setLayout(gl2);
+        p2 = new JPanel();
+        GridLayout gl2 = new GridLayout(4, 1);
+        p2.setLayout(gl2);
 
-p3 = new JPanel();
-GridLayout gl3 = new GridLayout(1,2);
-p3.setLayout(gl3);
+        p3 = new JPanel();
+        GridLayout gl3 = new GridLayout(1, 2);
+        p3.setLayout(gl3);
 
 // Добавляем кнопки на панель p1
 //for (int i=9; i>0; i--) p1.add(numButtons[i]);
 
-p1.add(numButtons[7]);
-p1.add(numButtons[8]);
-p1.add(numButtons[9]);
-p1.add(numButtons[4]);
-p1.add(numButtons[5]);
-p1.add(numButtons[6]);
-p1.add(numButtons[1]);
-p1.add(numButtons[2]);
-p1.add(numButtons[3]);
-p1.add(numButtons[0]);
-p1.add(buttonPoint);
-p1.add(buttonEqual);
-p2.add(buttonDivide);
-p2.add(buttonMultiply);
-p2.add(buttonMinus);
-p2.add(buttonPlus);
-p3.add(buttonSqrt);
-p3.add(buttonClear);
-p3.add(displayField2);
+        p1.add(numButtons[7]);
+        p1.add(numButtons[8]);
+        p1.add(numButtons[9]);
+        p1.add(numButtons[4]);
+        p1.add(numButtons[5]);
+        p1.add(numButtons[6]);
+        p1.add(numButtons[1]);
+        p1.add(numButtons[2]);
+        p1.add(numButtons[3]);
+        p1.add(numButtons[0]);
+        p1.add(buttonPoint);
+        p1.add(buttonEqual);
+        p2.add(buttonDivide);
+        p2.add(buttonMultiply);
+        p2.add(buttonMinus);
+        p2.add(buttonPlus);
+        p3.add(buttonSqrt);
+        p3.add(buttonClear);
+        p3.add(displayField2);
 
 // Создаем экземпляр слушателя событий и
 // регистрируем его в каждой кнопке
-CalculatorEngine calcEngine = new CalculatorEngine(this);
-for (int i=0; i<10; i++) numButtons[i].addActionListener(calcEngine);
-buttonPoint.addActionListener(calcEngine);
-buttonEqual.addActionListener(calcEngine);
-buttonPlus.addActionListener(calcEngine);
-buttonMinus.addActionListener(calcEngine);
-buttonDivide.addActionListener(calcEngine);
-buttonMultiply.addActionListener(calcEngine);
-buttonSqrt.addActionListener(calcEngine);
-buttonClear.addActionListener(calcEngine);
+        CalculatorEngine calcEngine = new CalculatorEngine(this);
+        for (int i = 0; i < 10; i++) numButtons[i].addActionListener(calcEngine);
+        buttonPoint.addActionListener(calcEngine);
+        buttonEqual.addActionListener(calcEngine);
+        buttonPlus.addActionListener(calcEngine);
+        buttonMinus.addActionListener(calcEngine);
+        buttonDivide.addActionListener(calcEngine);
+        buttonMultiply.addActionListener(calcEngine);
+        buttonSqrt.addActionListener(calcEngine);
+        buttonClear.addActionListener(calcEngine);
 
 // Помещаем панель p1, p2, p3 в области окна
-windowContent.add("Center",p1);
-windowContent.add("East",p2);
-windowContent.add("South",p3);
-displayField.setText("0");
+        windowContent.add("Center", p1);
+        windowContent.add("East", p2);
+        windowContent.add("South", p3);
+        displayField.setText("0");
 
 // Создаём фрейм и задаём его основную панель
-JFrame frame = new JFrame("Calculator");
-frame.setContentPane(windowContent);
+        JFrame frame = new JFrame("Calculator");
+        frame.setContentPane(windowContent);
 
 // делаем размер окна достаточным
 // для того, чтобы вместить все компоненты
-frame.pack();
-frame.setSize(230, (int) frame.getSize().getHeight());
+        frame.pack();
+        frame.setSize(230, (int) frame.getSize().getHeight());
 // Наконец, отображаем окно
-frame.setVisible(true);
-}
-	public static void main(String[] args) {
-		Calculator calc = new Calculator();
+        frame.setVisible(true);
+    }
 
-	}
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+    }
 }
--- src/org/calc/CalculatorEngine.java
+++ src/org/calc/CalculatorEngine.java
@@ -6,155 +6,156 @@ import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 
 public class CalculatorEngine implements ActionListener {
-	
-	Calculator parent;
-	
-	// Конструктор сохраняет ссылку на окно калькулятора
-	// в переменной экземпляра класса
-	CalculatorEngine(Calculator parent){
-		this.parent = parent;}
-	
-	char selectedAction = ' ';
-	double memoryResult = 0;
-	double currentResult = 0;
-	boolean byZero, onEqual = false;
 
-	public void actionPerformed(ActionEvent e){
-		
-		// Get source of event
-		JButton clickedButton = (JButton) e.getSource();
-		
-		String dispFieldText = parent.displayField.getText();
-		double displayValue = 0;
-		
-		if (!"".equals(dispFieldText)) {
-			if (!byZero) displayValue = Double.parseDouble(dispFieldText);
-			else {
-				displayValue = 0;
-				byZero = false;
-				selectedAction = ' ';
-				currentResult = 0;
-				memoryResult = 0;
-				parent.displayField2.setText("");
-			}
-		}
+    Calculator parent;
 
-		Object src = e.getSource();
-		
-		if (src == parent.buttonPlus){
-			selectedAction = '+';
-			currentResult = displayValue;
-			parent.displayField.setText("");
-			parent.displayField2.setText("+");
-			onEqual = false;
+    // Конструктор сохраняет ссылку на окно калькулятора
+    // в переменной экземпляра класса
+    CalculatorEngine(Calculator parent) {
+        this.parent = parent;
+    }
 
-		} else if (src == parent.buttonMinus){
-			selectedAction = '-';
-			currentResult = displayValue;
-			parent.displayField.setText("");
-			parent.displayField2.setText("-");
-			onEqual = false;
-		
-		} else if (src == parent.buttonDivide){
-			selectedAction = '/';
-			currentResult = displayValue;
-			parent.displayField.setText("");
-			parent.displayField2.setText("/");
-			onEqual = false;
-		
-		} else if (src == parent.buttonMultiply){
-			selectedAction = '*';
-			currentResult = displayValue;
-			parent.displayField.setText("");
-			parent.displayField2.setText("*");
-			onEqual = false;
-		
-		} else if (src == parent.buttonSqrt){
-			selectedAction = 'Q';
-			currentResult = Math.sqrt(displayValue);
-			memoryResult = currentResult;
-			parent.displayField.setText("" + currentResult);
-			parent.displayField2.setText("sqrt()");
-			onEqual = true;
-				
-		} else if (src == parent.buttonPoint){
-			if (onEqual) parent.displayField2.setText("");
-			
-			//Check for no more than one "." entered
-			String s = String.valueOf(dispFieldText+clickedButton.getText());
-			int counter = 0;
-			for (int i=0; i<s.length(); i++ ) {
-				if( s.charAt(i) == '.' ) {
-					counter++;
-					}
-				}
+    char selectedAction = ' ';
+    double memoryResult = 0;
+    double currentResult = 0;
+    boolean byZero, onEqual = false;
 
-			if (displayValue == 0 | onEqual) {
-				parent.displayField.setText("0.");
-				onEqual = false;
-				}
-			else if (counter == 1){ 
-				parent.displayField.setText(dispFieldText + clickedButton.getText());
-				}
-					
-		} else if (src == parent.buttonClear){
-			parent.displayField.setText("0");
-			parent.displayField2.setText("");
-			currentResult = 0;
-			memoryResult = 0;
-			selectedAction = ' ';
-			byZero = false;
-			onEqual = false;
-						
-		} else if (src == parent.buttonEqual){
-			onEqual = true;
-			parent.displayField2.setText("");
-			
-			if (selectedAction == '+'){
-				currentResult += memoryResult;
-				parent.displayField.setText("" + currentResult);
-			
-			} else if (selectedAction == '-'){
-				currentResult -= memoryResult;
-				parent.displayField.setText("" + currentResult);		
-			
-			} else if (selectedAction == ' '){
-				parent.displayField.setText("" + memoryResult);
-												
-			} else if (selectedAction == '*'){
-				currentResult *= memoryResult;
-				parent.displayField.setText("" + currentResult);
-								
-			// Checking if divide by 0
-			} else if (selectedAction == '/'){
-				if (memoryResult != 0) {
-					currentResult /= memoryResult;
-					parent.displayField.setText("" + currentResult);
-				} else {
-					parent.displayField.setText("Cannot divide by zero");
-					byZero = true;
-					}
-			}
-			
-		} else {
-			if (onEqual) {
-				dispFieldText = "";
-				onEqual = false;
-			}
+    public void actionPerformed(ActionEvent e) {
 
-			String clickedButtonLabel = clickedButton.getText();
-			
-			// Check for "00.." condition and transform it to "0"
-			if ((dispFieldText+clickedButtonLabel).length() > 1 & displayValue == 0 & 
-					String.valueOf(dispFieldText).indexOf('.') != 1) {
-				parent.displayField.setText(clickedButtonLabel);
-				memoryResult = Double.parseDouble(clickedButtonLabel);
-								
-			} else {
-				parent.displayField.setText(dispFieldText + clickedButtonLabel);
-				memoryResult = Double.parseDouble(dispFieldText + clickedButtonLabel);}
-			}
-		
-	} // end of actionPerformed method
-	
+        // Get source of event
+        JButton clickedButton = (JButton) e.getSource();
+
+        String dispFieldText = parent.displayField.getText();
+        double displayValue = 0;
+
+        if (!"".equals(dispFieldText)) {
+            if (!byZero) displayValue = Double.parseDouble(dispFieldText);
+            else {
+                displayValue = 0;
+                byZero = false;
+                selectedAction = ' ';
+                currentResult = 0;
+                memoryResult = 0;
+                parent.displayField2.setText("");
+            }
+        }
+
+        Object src = e.getSource();
+
+        if (src == parent.buttonPlus) {
+            selectedAction = '+';
+            currentResult = displayValue;
+            parent.displayField.setText("");
+            parent.displayField2.setText("+");
+            onEqual = false;
+
+        } else if (src == parent.buttonMinus) {
+            selectedAction = '-';
+            currentResult = displayValue;
+            parent.displayField.setText("");
+            parent.displayField2.setText("-");
+            onEqual = false;
+
+        } else if (src == parent.buttonDivide) {
+            selectedAction = '/';
+            currentResult = displayValue;
+            parent.displayField.setText("");
+            parent.displayField2.setText("/");
+            onEqual = false;
+
+        } else if (src == parent.buttonMultiply) {
+            selectedAction = '*';
+            currentResult = displayValue;
+            parent.displayField.setText("");
+            parent.displayField2.setText("*");
+            onEqual = false;
+
+        } else if (src == parent.buttonSqrt) {
+            selectedAction = 'Q';
+            currentResult = Math.sqrt(displayValue);
+            memoryResult = currentResult;
+            parent.displayField.setText("" + currentResult);
+            parent.displayField2.setText("sqrt()");
+            onEqual = true;
+
+        } else if (src == parent.buttonPoint) {
+            if (onEqual) parent.displayField2.setText("");
+
+            //Check for no more than one "." entered
+            String s = String.valueOf(dispFieldText + clickedButton.getText());
+            int counter = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '.') {
+                    counter++;
+                }
+            }
+
+            if (displayValue == 0 | onEqual) {
+                parent.displayField.setText("0.");
+                onEqual = false;
+            } else if (counter == 1) {
+                parent.displayField.setText(dispFieldText + clickedButton.getText());
+            }
+
+        } else if (src == parent.buttonClear) {
+            parent.displayField.setText("0");
+            parent.displayField2.setText("");
+            currentResult = 0;
+            memoryResult = 0;
+            selectedAction = ' ';
+            byZero = false;
+            onEqual = false;
+
+        } else if (src == parent.buttonEqual) {
+            onEqual = true;
+            parent.displayField2.setText("");
+
+            if (selectedAction == '+') {
+                currentResult += memoryResult;
+                parent.displayField.setText("" + currentResult);
+
+            } else if (selectedAction == '-') {
+                currentResult -= memoryResult;
+                parent.displayField.setText("" + currentResult);
+
+            } else if (selectedAction == ' ') {
+                parent.displayField.setText("" + memoryResult);
+
+            } else if (selectedAction == '*') {
+                currentResult *= memoryResult;
+                parent.displayField.setText("" + currentResult);
+
+                // Checking if divide by 0
+            } else if (selectedAction == '/') {
+                if (memoryResult != 0) {
+                    currentResult /= memoryResult;
+                    parent.displayField.setText("" + currentResult);
+                } else {
+                    parent.displayField.setText("Cannot divide by zero");
+                    byZero = true;
+                }
+            }
+
+        } else {
+            if (onEqual) {
+                dispFieldText = "";
+                onEqual = false;
+            }
+
+            String clickedButtonLabel = clickedButton.getText();
+
+            // Check for "00.." condition and transform it to "0"
+            if ((dispFieldText + clickedButtonLabel).length() > 1 & displayValue == 0 &
+                    String.valueOf(dispFieldText).indexOf('.') != 1) {
+                parent.displayField.setText(clickedButtonLabel);
+                memoryResult = Double.parseDouble(clickedButtonLabel);
+
+            } else {
+                parent.displayField.setText(dispFieldText + clickedButtonLabel);
+                memoryResult = Double.parseDouble(dispFieldText + clickedButtonLabel);
+            }
+        }
+
+    } // end of actionPerformed method
+
 } // end of CalculatorEngine class
